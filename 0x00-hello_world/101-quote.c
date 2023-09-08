103fac6a97a985f28d8609ffbbb9a684f4442508
yabsera <yabseraseyfe@gmail.com>
2023-09-07 23:34:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 /**
-*main - is the main function of our program
+*main - function of program
 *Description: print the text without use printf, put
 *Return: one
 */
 int main(void)
 {
-	char *word = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	while (*word) putchar (*word++);
-	return (1);
+char *word = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+while (*word)
+putchar (*word++);
+return (1);
 }
