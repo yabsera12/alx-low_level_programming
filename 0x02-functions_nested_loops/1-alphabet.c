1f437480776b23fcecda9eeae7b53c42f267e657
yabsera <yabseraseyfe@gmail.com>
2023-09-13 20:01:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - code that print alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+}
