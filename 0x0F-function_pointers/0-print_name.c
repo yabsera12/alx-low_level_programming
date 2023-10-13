e9542eba6c0882a38628baaf0c942e83c71bc907
yabsera12 <yabfeker633@gmail.com>
2023-10-13 00:22:00 -0700

    prototype changed to function_pointers.h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include "stddef.h"
-#include "main.h"
+#include "function_pointers.h"
 
 /**
- * print_name - writes the character c to stdout
- * @name: String
- * @f: Pointer to function with char parameter and no return
- *
- * Return: Nothing
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != 0)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: main.h
- * AUth: yabsera seyfe
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
- */
-
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
