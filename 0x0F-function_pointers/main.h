547d3b6418603c170dcc51a2fc7050e064774e6e
yabsera12 <yabfeker633@gmail.com>
2023-10-13 00:13:43 -0700

    main.h edietd

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,4 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * AUth: yabsera seyfe
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+#endif
