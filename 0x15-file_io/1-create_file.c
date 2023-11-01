4b8297e77721c04ecd0dca52f3dcd26b14f896bf
yabsera12 <yabfeker633@gmail.com>
2023-10-31 23:48:14 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,28 @@
 #include "main.h"
-
 /**
- * create_file - creates a file
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
- */
+  * create_file - A function creates a file with rw  permissions
+  * @filename: name of the file, if NULL, return -1
+  * @text_content: contents of the file. If NULL, create an empty file
+  * Return: 1 on success, -1 on failure
+  */
 int create_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int new_file, length, wr_stat;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-    if (file == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (new_file == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (1);
+	}
+	for (length = 0; text_content[length]; length++)
+		;
+	wr_stat = write(new_file, text_content, length);
+	if (close(new_file) == -1)
+		return (-1);
+	return (wr_stat == -1 ? -1 : 1);
 }
